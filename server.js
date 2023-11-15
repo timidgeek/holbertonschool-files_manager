a4770a9b6f4b5fd857a16b57b3bbea925d3e0e8e
hayes28 <heather.hayes918@gmail.com>
2023-11-15 16:53:39 -0600

    Task 2 Add endpoints and routes for API

---
+++ server.js
@@ -0,0 +1,17 @@
+// Task 2. First API
+const express = require('express');
+const app = express();
+
+// Load routes
+const routes = require('./routes/index');
+
+// Middleware to use the routes
+app.use(routes);
+
+// Listen on port from .env
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log('Server running on port 5000');
+});
+
+module.exports = app;
