609fcb69057015dd1d8bfea34f0ecabea4c27b48
timidgeek <9liveslinds@gmail.com>
2023-11-17 12:46:20 -0800

    full routes index

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,30 @@
 // All endpoints for the API are defined here
-
 const express = require('express');
 
 const router = express.Router();
-const AppController = require('../controllers/AppController');
 
+// controllers
+const AppController = require('../controllers/AppController');
+const AuthController = require('../controllers/AuthController');
+const FilesController = require('../controllers/FilesController');
+const UsersController = require('../controllers/UsersController');
+
+// get routes
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.get('/files/:id/data', FilesController.getFile);
+
+// post routes
+router.post('/users', UsersController.postNew);
+router.post('/files', FilesController.postUpload);
+
+// put routes
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/publish', FilesController.putUnpublish);
 
 module.exports = router;
