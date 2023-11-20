1b50332985feef529d3ff5244e171776be5ad35a
hayes28 <heather.hayes918@gmail.com>
2023-11-19 18:44:12 -0600

    uncommented routes for task 5 and 6

--- routes/index.js
+++ routes/index.js
@@ -16,15 +16,15 @@ router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
 router.get('/files/:id', FilesController.getShow);
-// router.get('/files', FilesController.getIndex);
-// router.get('/files/:id/data', FilesController.getFile);
+router.get('/files', FilesController.getIndex);
+router.get('/files/:id/data', FilesController.getFile);
 
 // post routes
 router.post('/users', UsersController.postNew);
 router.post('/files', FilesController.postUpload);
 
 // put routes
-// router.put('/files/:id/publish', FilesController.putPublish);
-// router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
 module.exports = router;
