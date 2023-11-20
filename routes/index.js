9dc7ebc32a5320a163e7d12f6524cf9d0bf698d9
hayes28 <heather.hayes918@gmail.com>
2023-11-19 21:23:00 -0600

    Fix Redis set duration parameter I got all the outputs to match the project example

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 // All endpoints for the API are defined here
 const express = require('express');
 
-const router = express.Router();
-
 // controllers
 const AppController = require('../controllers/AppController');
 const AuthController = require('../controllers/AuthController');
 const FilesController = require('../controllers/FilesController');
 const UsersController = require('../controllers/UsersController');
+
+const router = express.Router();
 
 // get routes
 router.get('/status', AppController.getStatus);
@@ -15,16 +15,16 @@ router.get('/stats', AppController.getStats);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
-router.get('/files/:id', FilesController.getShow);
-router.get('/files', FilesController.getIndex);
-router.get('/files/:id/data', FilesController.getFile);
+// router.get('/files/:id', FilesController.getShow);
+// router.get('/files', FilesController.getIndex);
+// router.get('/files/:id/data', FilesController.getFile);
 
 // post routes
 router.post('/users', UsersController.postNew);
 router.post('/files', FilesController.postUpload);
 
 // put routes
-router.put('/files/:id/publish', FilesController.putPublish);
-router.put('/files/:id/unpublish', FilesController.putUnpublish);
+// router.put('/files/:id/publish', FilesController.putPublish);
+// router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
 module.exports = router;
