f78be1cbfdef7d4b4e8a4e67454129c68c93d77f
hayes28 <heather.hayes918@gmail.com>
2023-11-20 14:30:25 -0600

    I am over this task. It is working in terminal. Losing more and more checks.

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -4,11 +4,11 @@ const { v4: uuid } = require('uuid');
 const redis = require('../utils/redis');
 const dbClient = require('../utils/db');
 
-class AuthController {
-  // GET req - sign in user
-  static async getConnect(req, res) {
-    const authHeader = req.headers.authorization;
 
+const AuthController = {
+  // GET req - sign in user
+  async getConnect(req, res) {
+    const authHeader = req.headers.authorization;
     // confirm request is Basic Auth compliant
     if (!authHeader || !authHeader.startsWith('Basic ')) {
       return res.status(401).json({ error: 'Unauthorized' });
@@ -19,38 +19,27 @@ class AuthController {
     const decode = Buffer.from(credentials, 'base64').toString();
     const [email, password] = decode.split(':');
 
-    // confirm email and password are present
     if (!email || !password) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
 
     // hash that pass, get user
-    try {
-      const hashedPassword = sha1(password);
-      const user = await dbClient.users.findOne({ email, password: hashedPassword });
+    const hashedPassword = sha1(password);
+    const user = await dbClient.users.findOne({ email, password: hashedPassword });
 
-      if (!user) {
-        return res.status(401).json({ error: 'Unauthorized' });
-      }
-
-      // generate token, store in redis for 24 hours
-      const token = uuid();
-      await redis.set(`auth_${token}`, user._id.toString(), 86400)
-        .catch((err) => {
-          console.error(err);
-          throw new Error('Redis set error');
-        });
-
-      // return specified token
-      return res.status(200).json({ token });
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ error: 'Internal Server Error' });
+    if (!user || user.password !== hashedPassword) {
+      return res.status(401).json({ error: 'Unauthorized' });
     }
-  }
+
+    // generate token, store in redis for 24 hours
+    const token = uuid();
+    await redis.set(`auth_${token}`, user._id.toString(), 86400)
+
+    return res.status(200).json({ token });
+  },
 
   // GET req - disconnect
-  static async getDisconnect(req, res) {
+  async getDisconnect(req, res) {
     const token = req.header('X-Token');
     const user = await redis.get(`auth_${token}`);
     // retrieve
